08305f11e3a06090ac54afde5bf5ec3604e333ba
rolphy <martinrh83@gmail.com>
2014-11-28 09:42:39 -0300

    version inicial

---
+++ js/hobbies.js
@@ -0,0 +1,21 @@
+  google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      function drawChart() {
+
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Facultad',      10],
+          ['Series de TV',     1],
+          ['Vicio',  1],
+          ['Sleep',    4],
+          ['PC',10]
+        ]);
+
+        var options = {
+       
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
